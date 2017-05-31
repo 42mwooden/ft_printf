7c8d538c8202a3fdb5c3531a8e7bc796e12083d5
Mitchell Wooden <mwooden@e1z2r7p1.42.us.org>
2017-05-31 03:27:25 -0700

    still

--- srcs/unsigned_handlers.c
+++ srcs/unsigned_handlers.c
@@ -30,7 +30,7 @@ ssize_t		uint_handler(uintmax_t num, t_flag *flag, char *base, char *sign)
 	if (flag->has_preci)
 		flag->pad_zero = 0;
 	num_len = numlen(num, base);
-	if (flag->has_width && !flag->pad_zero)
+	if (flag->has_width && !flag->pad_right &&flag->pad_zero)
 	{
 		if (flag->has_preci)
 			flag->preci = my_greater(flag->width, flag->preci);
